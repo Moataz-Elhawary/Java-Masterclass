58e4922dc2090777cf28117a24f02bfab7df251c
Moataz Elhawary <mm201797@gmail.com>
2025-05-25 09:23:28 +0300

    Upload Ex_38

---
+++ Section_8/src/CodingExercise_38/Main.java
@@ -0,0 +1,23 @@
+package CodingExercise_38;
+
+public class Main {
+    public static void main(String[] args) {
+        // TEST CODE
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("YOUR NAME HERE", wall1, wall2, wall3, wall4, ceiling,bed, lamp);
+        bedRoom.makeBed();
+
+        bedRoom.getLamp().turnOn();
+    }
+}
